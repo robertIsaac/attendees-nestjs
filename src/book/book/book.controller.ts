22e6e6efd10a3b3de36b541a38ada45f4277fcf4
robertIsaac <robert.isaac@live.com>
2021-04-13 20:21:57 +0200

    add guard to the book controller

--- src/book/book/book.controller.ts
+++ src/book/book/book.controller.ts
@@ -10,12 +10,14 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Status } from '../interfaces/status';
 import { ArabicDayOfWeekEnum, DayOfWeekEnum } from '../interfaces/day-of-week.enum';
 import { BookEntity } from '../book.entity';
 import { I18n, I18nContext, I18nLang } from 'nestjs-i18n';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('book')
 export class BookController {
@@ -90,22 +92,26 @@ export class BookController {
     throw new HttpException('something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get('dates')
   async dates() {
     const dates = (await this.bookService.getDates()).map((book) => new Date(book.massTime).toISOString());
     return [...new Set(dates)];
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get('attendees')
   async attendees(@Query('massTime') massTime: string) {
     return this.bookService.attendees(massTime);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get('attendanceByPhone/:phone')
   async attendanceByPhone(@Param('phone') phone: string) {
     return this.bookService.getAttendanceByPhone(phone);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   async findOne(@Param('id') id: string, @I18n() i18n: I18nContext): Promise<BookEntity> {
     const book = await this.bookService.getBook(id);
@@ -116,6 +122,7 @@ export class BookController {
     return book;
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Put(':id')
   @HttpCode(204)
   async update(@Param('id') id: string, @Body() bookEntity: BookEntity, @I18n() i18n: I18nContext): Promise<void> {
@@ -141,6 +148,7 @@ export class BookController {
     return;
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   @HttpCode(204)
   async remove(@Param('id') id: string, @I18n() i18n: I18nContext): Promise<void> {
